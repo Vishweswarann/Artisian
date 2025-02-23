49be5e58634b78c0ad38aebba8db9eda76b72a17
Vishweswaran <vishweswarann.24cse@kongu.edu>
2025-02-23 06:49:13 +0000

    connected the database with frontend in user page

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
+import requests
+from flask import Flask, render_template, request, url_for, flash, redirect, session
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+from datetime import date, datetime
 import os
 
 
@@ -8,6 +11,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "vanakam"
 uploadFolder = "static/uploads"
+
+app.secret_key = "vanakam"
 
 os.makedirs(uploadFolder, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = uploadFolder
@@ -19,7 +24,7 @@ db = SQLAlchemy(app)
 
 
 class Users(db.Model):
-    _id = db.Column("id", db.Integer, primary_key=True)
+    id = db.Column("id", db.Integer, primary_key=True)
     name = db.Column("name", db.String(100))
     email = db.Column("email", db.String(100))
     password = db.Column("password", db.String(100))
@@ -30,28 +35,232 @@ class Users(db.Model):
         self.password = password
 
 
-class Product(db.Model):
-    _id = db.Column("id", db.Integer, primary_key=True)
-    productCategory = db.Column("productCategory ", db.String(100))
-    productName = db.Column("productName", db.String(100))
-    productDescription = db.Column("productDescription", db.String(300))
-    productQuantity = db.Column("productQuantity", db.Integer)
-    productPrice = db.Column("productPrice",  db.Integer)
-    productAddress = db.Column("productAddress", db.String(100))
-    productPincode = db.Column("productPincode", db.Integer)
+# class Product(db.Model):
+#     _id = db.Column("id", db.Integer, primary_key=True)
+#     productCategory = db.Column("productCategory ", db.String(100))
+#     productName = db.Column("productName", db.String(100))
+#     productDescription = db.Column("productDescription", db.String(300))
+#     productQuantity = db.Column("productQuantity", db.Integer)
+#     productPrice = db.Column("productPrice",  db.Integer)
+#     productAddress = db.Column("productAddress", db.String(100))
+#     productPincode = db.Column("productPincode", db.Integer)
+#
+#     def __init__(self, productCategory, productName, productDescription,productQuantity, productPrice, productAddress, productPincode):
+#         self.productCategory = productCategory
+#         self.productName = productName
+#         self.productDescription = productDescription
+#         self.productQuantity = productQuantity
+#         self.productPrice = productPrice
+#         self.productAddress = productAddress
+#         self.productPincode = productPincode
+#
 
-    def __init__(self, productCategory, productName, productDescription,productQuantity, productPrice, productAddress, productPincode):
-        self.productCategory = productCategory
-        self.productName = productName
-        self.productDescription = productDescription
-        self.productQuantity = productQuantity
-        self.productPrice = productPrice
-        self.productAddress = productAddress
-        self.productPincode = productPincode
+class pottery(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer,)
+    Name = db.Column(db.String(100))
+    Description = db.Column(db.String(500))
+    Quantity = db.Column(db.Integer)
+    Price = db.Column(db.Integer)
+    Width = db.Column(db.Integer)
+    Height = db.Column(db.Integer)
+    Length = db.Column(db.Integer)
+    Address = db.Column(db.String(400))
+    Contact = db.Column(db.Integer)
+    Image = db.Column(db.String(100))
+
+            # r = pottery(session["userId"], proName, proDesription, proQuantity, proPrice, proAddress,proHeight, proWidth,pro, proContact)
+    def __init__(self,userId, productName, productDescription,productQuantity, productPrice, productAddress,productHeight,productWidth,productLength, productContact, productImage):
+        self.userId = userId
+        self.Name = productName
+        self.Description = productDescription
+        self.Quantity = productQuantity
+        self.Price = productPrice
+        self.Address = productAddress
+        self.Height = productHeight
+        self.Width = productWidth
+        self.Length = productLength
+        self.Contact = productContact
+        self.Image = productImage
     
+class weaving(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer,)
+    Name = db.Column(db.String(100))
+    Description = db.Column(db.String(500))
+    Quantity = db.Column(db.Integer)
+    Price = db.Column(db.Integer)
+    Width = db.Column(db.Integer)
+    Height = db.Column(db.Integer)
+    Length = db.Column(db.Integer)
+    Address = db.Column(db.String(400))
+    Contact = db.Column(db.Integer)
+    Image = db.Column(db.String(100))
+
+            # r = pottery(session["userId"], proName, proDesription, proQuantity, proPrice, proAddress,proHeight, proWidth,prolength, proContact)
+    def __init__(self,userId, productName, productDescription,productQuantity, productPrice, productAddress,productHeight,productWidth,productLength, productContact, productImage):
+        self.userId = userId
+        self.Name = productName
+        self.Description = productDescription
+        self.Quantity = productQuantity
+        self.Price = productPrice
+        self.Address = productAddress
+        self.Height = productHeight
+        self.Width = productWidth
+        self.Length = productLength
+        self.Contact = productContact
+        self.Image = productImage
+ 
+
+class carpentry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer,)
+    Name = db.Column(db.String(100))
+    Description = db.Column(db.String(500))
+    Quantity = db.Column(db.Integer)
+    Price = db.Column(db.Integer)
+    Width = db.Column(db.Integer)
+    Height = db.Column(db.Integer)
+    Length = db.Column(db.Integer)
+    Address = db.Column(db.String(400))
+    Contact = db.Column(db.Integer)
+    Image = db.Column(db.String(100))
+
+            # r = pottery(session["userId"], proName, proDesription, proQuantity, proPrice, proAddress,proHeight, proWidth,pro, proContact)
+    def __init__(self,userId, productName, productDescription,productQuantity, productPrice, productAddress,productHeight,productWidth,productLength, productContact, productImage):
+        self.userId = userId
+        self.Name = productName
+        self.Description = productDescription
+        self.Quantity = productQuantity
+        self.Price = productPrice
+        self.Address = productAddress
+        self.Height = productHeight
+        self.Width = productWidth
+        self.Length = productLength
+        self.Contact = productContact
+        self.Image = productImage
+
+
+
+class blacksmith(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer,)
+    Name = db.Column(db.String(100))
+    Description = db.Column(db.String(500))
+    Quantity = db.Column(db.Integer)
+    Price = db.Column(db.Integer)
+    Width = db.Column(db.Integer)
+    Height = db.Column(db.Integer)
+    Length = db.Column(db.Integer)
+    Address = db.Column(db.String(400))
+    Contact = db.Column(db.Integer)
+    Image = db.Column(db.String(100))
+
+            # r = pottery(session["userId"], proName, proDesription, proQuantity, proPrice, proAddress,proHeight, proWidth,proLength, proContact)
+    def __init__(self,userId, productName, productDescription,productQuantity, productPrice, productAddress,productHeight,productWidth,productLength, productContact, productImage):
+        self.userId = userId
+        self.Name = productName
+        self.Description = productDescription
+        self.Quantity = productQuantity
+        self.Price = productPrice
+        self.Address = productAddress
+        self.Height = productHeight
+        self.Width = productWidth
+        self.Length = productLength
+        self.Contact = productContact
+        self.Image = productImage
+
+
+class sculpture(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer,)
+    Name = db.Column(db.String(100))
+    Description = db.Column(db.String(500))
+    Quantity = db.Column(db.Integer)
+    Price = db.Column(db.Integer)
+    Width = db.Column(db.Integer)
+    Height = db.Column(db.Integer)
+    Length = db.Column(db.Integer)
+    Address = db.Column(db.String(400))
+    Contact = db.Column(db.Integer)
+    Image = db.Column(db.String(100))
+
+            # r = pottery(session["userId"], proName, proDesription, proQuantity, proPrice, proAddress,proHeight, proWidth,proLength, proContact)
+    def __init__(self,userId, productName, productDescription,productQuantity, productPrice, productAddress,productHeight,productWidth,productLength, productContact, productImage):
+        self.userId = userId
+        self.Name = productName
+        self.Description = productDescription
+        self.Quantity = productQuantity
+        self.Price = productPrice
+        self.Address = productAddress
+        self.Height = productHeight
+        self.Width = productWidth
+        self.Length = productLength
+        self.Contact = productContact
+        self.Image = productImage
+
+
+class painting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer,)
+    Name = db.Column(db.String(100))
+    Description = db.Column(db.String(500))
+    Quantity = db.Column(db.Integer)
+    Price = db.Column(db.Integer)
+    Width = db.Column(db.Integer)
+    Height = db.Column(db.Integer)
+    Length = db.Column(db.Integer)
+    Address = db.Column(db.String(400))
+    Contact = db.Column(db.Integer)
+    Image = db.Column(db.String(100))
+
+            # r = pottery(session["userId"], proName, proDesription, proQuantity, proPrice, proAddress,proHeight, proWidth,proLength, proContact)
+    def __init__(self,userId, productName, productDescription,productQuantity, productPrice, productAddress,productHeight,productWidth,productLength, productContact, productImage):
+        self.userId = userId
+        self.Name = productName
+        self.Description = productDescription
+        self.Quantity = productQuantity
+        self.Price = productPrice
+        self.Address = productAddress
+        self.Height = productHeight
+        self.Width = productWidth
+        self.Length = productLength
+        self.Contact = productContact
+        self.Image = productImage
+
+
+
+class other(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer)
+    Name = db.Column(db.String(100))
+    Description = db.Column(db.String(500))
+    Quantity = db.Column(db.Integer)
+    Price = db.Column(db.Integer)
+    Width = db.Column(db.Integer)
+    Height = db.Column(db.Integer)
+    Length = db.Column(db.Integer)
+    Address = db.Column(db.String(400))
+    Contact = db.Column(db.Integer)
+    Image = db.Column(db.String(100))
+
+            # r = pottery(session["userId"], proName, proDesription, proQuantity, proPrice, proAddress,proHeight, proWidth,proLength, proContact)
+    def __init__(self,userId, productName, productDescription,productQuantity, productPrice, productAddress,productHeight,productWidth,productLength, productContact, productImage):
+        self.userId = userId
+        self.Name = productName
+        self.Description = productDescription
+        self.Quantity = productQuantity
+        self.Price = productPrice
+        self.Address = productAddress
+        self.Height = productHeight
+        self.Width = productWidth
+        self.Length = productLength
+        self.Contact = productContact
+        self.Image = productImage
 
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template("home.html")
 
@@ -90,10 +299,12 @@ def login():
         found_user = Users.query.filter_by(email = email).first()
         if found_user:
             if found_user.password == password:
+                session["userId"] = found_user.id
+                print(session["userId"])
                 return redirect(url_for('user'))
     
             else:
-                print("hello")
+                # print("hello")
                 flash("Invalid User credentials")
                 return redirect(url_for('login'))
         else:
@@ -106,31 +317,77 @@ def login():
 
 @app.route("/user", methods = ["POST", "GET"])
 def user():
-    return render_template("user.html")
+    potteryProducts = pottery.query.all()
+    return render_template("user.html", potteryProducts = potteryProducts)
 
-@app.route("/add", methods = ["POST", "GET"])
-def add():
+@app.route("/sell", methods = ["POST", "GET"])
+def sell():
     if request.method == "POST":
-        file = request.files["images"]
-        file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
+        img = request.files["images"]
         category = request.form.get("category")
         proName = request.form.get("name")
         proDesription = request.form.get("description")
         proQuantity = request.form.get("quantity")
+        proHeight = request.form.get("height")
+        proWidth = request.form.get("width")
+        proLength = request.form.get("length")
         proPrice = request.form.get("price")
         proAddress = request.form.get("address")
-        proPincode = request.form.get("pincode")
+        proContact = request.form.get("phoneNumber")
+
+
+
+        # gets the extension of the image file and stores it in ext
+        ext = os.path.splitext(img.filename)[1]
+
+        # creates a new filename with the name as date and time 
+
+        new_imgname = f"{category}_{datetime.now().strftime('%Y%m%d%H%M%S')}{ext}"
+        safe_imgname = secure_filename(new_imgname)
+
+        img_path = app.config["UPLOAD_FOLDER"] + '/' + safe_imgname
+        img.save(img_path)
+
+        print(img_path)
+
+
 
         print(category)
-        r = Product(category, proName, proDesription, proQuantity, proPrice, proAddress, proPincode)
+
+        if category == "pottery":
+            r = pottery(session["userId"], proName, proDesription, proQuantity, proPrice, proAddress,proHeight, proWidth,proLength, proContact,img_path)
+
+        elif category == "weaving":
+            r = weaving(session["userId"], proName, proDesription, proQuantity, proPrice, proAddress,proHeight, proWidth,proLength, proContact,img_path)
+
+        elif category == "carpenrtry":
+            r = carpentry(session["userId"], proName, proDesription, proQuantity, proPrice, proAddress,proHeight, proWidth,proLength, proContact,img_path)
+
+        elif category == "blacksmith":
+            r = blacksmith(session["userId"], proName, proDesription, proQuantity, proPrice, proAddress,proHeight, proWidth,proLength, proContact,img_path)
+
+        elif category == "sculpture":
+            r = sculpture(session["userId"], proName, proDesription, proQuantity, proPrice, proAddress,proHeight, proWidth,proLength, proContact,img_path)
+
+        elif category == "painting":
+            r = painting(session["userId"], proName, proDesription, proQuantity, proPrice, proAddress,proHeight, proWidth,proLength, proContact,img_path)
+
+        elif category == "other":
+            r = other(session["userId"], proName, proDesription, proQuantity, proPrice, proAddress,proHeight, proWidth,proLength, proContact,img_path)
+
+        else:
+            return redirect(url_for('sell'))
+        
+
         db.session.add(r)
         db.session.commit()
         flash("succesfully added")
-        return redirect("url_for('add')")
+        print("succesfully added")
+        return redirect(url_for('sell'))
 
 
         
-    return render_template("productAdd.html")
+    return render_template("productSell.html")
 
 
 
@@ -149,6 +406,7 @@ def query_all():
     if all:
         for user in all:
             print(f"ID: {user._id}, Name: {user.name}, Email: {user.email}, Password: {user.password}")
+
     
     return render_template("admin.html")
     
