7f2ddf465494e37de9d8048637c055fadc542435
Vishweswaran <vishweswarann.24cse@kongu.edu>
2025-02-25 00:37:28 +0000

    added the buyNow page and the allProducts page

--- main.py
+++ main.py
@@ -482,5 +482,5 @@ def buy():
 if __name__ == "__main__":
     with app.app_context():  # Ensure db.create_all() runs in the app context
         db.create_all()
-        # app.run(host="0.0.0.0", port=5000)
-        app.run(debug=True)
+        app.run(host="0.0.0.0", port=5000)
+        # app.run(debug=True)
