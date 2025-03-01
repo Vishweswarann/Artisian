73b582b17943a34a0dcb51b3118ca4a8d28ed31c
Vishweswaran <vishweswarann.24cse@kongu.edu>
2025-03-01 00:56:39 +0000

    created requirements.txt

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import os
 
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'users3.sqlite3')
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite3"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "vanakam"
